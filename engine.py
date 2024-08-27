0b9c0fc140465f58866db35c598d3f620559670d
Abdullah <boltholt17@gmail.com>
2024-08-27 11:44:37 +0700

    add transfer learning with vitB16 pretrained model

--- engine.py
+++ engine.py
@@ -118,7 +118,7 @@ def train(model: torch.nn.Module,
          loss_fn: torch.nn.Module, 
          epochs: int, 
          device: torch.device, 
-         writer: torch.utils.tensorboard.writer.SummaryWriter) -> Dict[str, List]:
+         writer: torch.utils.tensorboard.writer.SummaryWriter = None) -> Dict[str, List]:
     """Trains and tests pytorch model
 
     passes a target model through train_step() and test_step() 
